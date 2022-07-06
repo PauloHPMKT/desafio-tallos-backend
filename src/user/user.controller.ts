742305acee167dd996e3d6041e1577b5ef03c3dd
Paulo Sergio <pauloserg861@gmail.com>
2022-07-06 15:26:34 -0300

    instalando biblioteca

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -19,11 +19,11 @@ export class UserController {
     return this.userService.findAll();
   }
 
-  @Get(':id')
+  /*@Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
-  }
-
+  }*/
+  
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
