df8bd19c9533ff623989f13757c7ff2390fcf166
Paulo Sergio <pauloserg861@gmail.com>
2022-07-06 16:26:00 -0300

    implementando validação ao dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, Matches } from "class-validator";
 
 export class CreateUserDto {
+    @IsNotEmpty({ message: 'Digite um nome de usuário valido' })
     name: string;
+
+    @IsEmail({}, { message: 'Digite um e-mail valido' })
     email: string;
+
+    @IsString()
+    @Matches(/((?=.*\d)|(?=.*\d+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).$/, {
+        message: 'a senha não é forte o bastante!',
+    })
     password: string;
     rule: string;
 }
