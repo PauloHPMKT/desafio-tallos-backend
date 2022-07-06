344b8f8195c576d457d848e6b990f915d8d4753d
Paulo Sergio <pauloserg861@gmail.com>
2022-07-06 15:58:39 -0300

    corrigindo metodo da classe de crypto

--- src/helpers/user.crypto.ts
+++ src/helpers/user.crypto.ts
@@ -2,7 +2,7 @@ import * as bcrypt from 'bcrypt'
 
 export class Encript {
     static async CriptoPass(pass: string): Promise<string> {
-        const salt = await bcrypt.geSalt()
+        const salt = await bcrypt.genSalt()
         return bcrypt.hash(pass, salt)
     }
 }
