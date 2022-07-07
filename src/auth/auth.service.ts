7e38704b364befd48765f513f45f3c85751653ae
Paulo Sergio <pauloserg861@gmail.com>
2022-07-07 08:29:53 -0300

    implementando estrategia local e validação

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,19 +5,21 @@ import { UserService } from 'src/user/user.service';
 @Injectable()
 export class AuthService {
     constructor(private readonly userService: UserService) {}
+
     // realizar login
     login() {
-        throw new Error('Method not implemented.');
+       return 'usuario logado';
     }
 
     //validar usuario
     async validateUser(email: string, password: string) {
         const user = await this.userService.findByEmail(email)
+    
         if(user) {
-            const validPassword = user.password = await Encript.ComparePass(password, user.password)
+            const compareValidPassword = user.password = await Encript.ComparePass(password, user.password)
 
-            if(validPassword) {
-                return user
+            if(compareValidPassword) {
+                return ({ user })
             }
         }
         throw new Error('Email ou senhas incorretos!')
