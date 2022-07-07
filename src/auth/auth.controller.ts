176a4b81f9d9c82df26e8d58846221602f64f6a2
Paulo Sergio <pauloserg861@gmail.com>
2022-07-07 10:29:49 -0300

    implementando currentuser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/currentuser.decorator';
 import { IsPublic } from './decorators/is-public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/auth.request';
@@ -8,11 +10,20 @@ import { AuthRequest } from './models/auth.request';
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
     
+    //login
     @IsPublic()
     @UseGuards(LocalAuthGuard)
     @HttpCode(HttpStatus.OK)
     @Post('login')
     login(@Request() req: AuthRequest) {
         return this.authService.login(req.user)
+    }
+
+    //authenticate me
+    @IsPublic()
+    @Get('me')
+    authenticateUser(@CurrentUser() user: User) {
+        console.log(user)
+        return user
     }
 }
