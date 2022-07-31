5a5c2a89c8461c282b3179c6eddda89d716d982c
Paulo Sergio <pauloserg861@gmail.com>
2022-07-30 21:57:00 -0300

    realizando ajustes nos services da API

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -1,7 +1,7 @@
 import * as bcrypt from 'bcrypt'
 
 export class Encript {
-    static async CriptoPass(pass: string): Promise<string> {
+    static async CriptoPass(pass: string) {
         const salt = await bcrypt.genSalt()
         return bcrypt.hash(pass, salt)
     }
