d77bcd5ef27d6ccf74c50dde299684b3dfaa5241
Paulo Sergio <pauloserg861@gmail.com>
2022-08-05 14:35:02 -0300

    refatorando estrutura com eslint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ServiceGateway } from 'src/gateway/service.gateway';
 import { AuthService } from './auth.service';
@@ -8,19 +15,19 @@ import { AuthRequest } from './models/auth.request';
 
 @Controller('api')
 export class AuthController {
-    constructor(
-        private readonly authService: AuthService,
-        private readonly serviceGateway: ServiceGateway,
-        ) {}
-    
-    //login
-    @ApiTags('login')
-    @IsPublic()
-    @UseGuards(LocalAuthGuard)
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    login(@Request() req: AuthRequest) {
-        this.serviceGateway.emitUserLoginEvent()
-        return this.authService.login(req.user)
-    }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly serviceGateway: ServiceGateway,
+  ) {}
+
+  //login
+  @ApiTags('login')
+  @IsPublic()
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  login(@Request() req: AuthRequest) {
+    this.serviceGateway.emitUserLoginEvent();
+    return this.authService.login(req.user);
+  }
 }
