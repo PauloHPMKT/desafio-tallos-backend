b3a885d150c882002e43148c93952bc77ba920b8
Paulo Sergio <pauloserg861@gmail.com>
2023-01-13 12:20:49 -0300

    implementando campos para o schema

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -1,10 +1,29 @@
 import * as mongoose from 'mongoose';
 
 export const UserSchema = new mongoose.Schema({
-  id: String,
   name: String,
   email: String,
+  contact: String,
+  cpf: String,
+  register_code: { type: Number, unique: true },
+  description: String,
   password: String,
+  location: {
+    address: {
+      street: String,
+      complement: String,
+      zipcode: String,
+      neighborhood: String,
+      city: String,
+      country: String,
+    },
+  },
+  corparative_info: {
+    office: String,
+    dept: String,
+    corporative_mail: String,
+    sub_dept: String,
+  },
   rules: String,
-  createdAt: { type: Date, default: Date.now() },
+  createdAt: { type: Date, default: Date.now },
 });
